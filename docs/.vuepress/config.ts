935d933fefe464dfdac0de6e17a2bcb5e34ed628
hajun-myoung <audgkwns44@gmail.com>
2021-07-07 16:50:35 +0900

    Err Fix : move base and dest

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,14 +5,14 @@ export default defineUserConfig<DefaultThemeOptions>({
   lang: 'ko-KR',
   title: '동두천시 코로나19 선별진료소 이용안내',
   description: '동두천시 코로나19 선별진료소 및 관련업무를 안내드리는 페이지입니다.',
+  base: '/covid-19-notice/',
+  dest: './dist',
 
   themeConfig: {
     logo: './image/logo.png',
     lastUpdated: true,
     contributors: false,
     home: '/',
-    base: '/covid-19-notice/',
-    dest: './dist',
     notFound: ['없는 페이지입니다.'],
     backToHome: '홈 화면으로',
     navbar: [
